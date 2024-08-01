ed7861f25a0071573e53c9675acb940258f7a80e
Rodney Kimathi <rodneykimathi@gmail.com>
2024-08-01 23:44:42 +0300

    Randomize recommendation based on priority weight

--- lib/ex_assignment/todos.ex
+++ lib/ex_assignment/todos.ex
@@ -49,9 +49,28 @@ defmodule ExAssignment.Todos do
     |> case do
       [] -> nil
       todos ->
-        min_priority = Enum.min_by(todos, &(&1.priority)).priority
-        min_priority_todos = Enum.filter(todos, &(&1.priority == min_priority))
-        Enum.random(min_priority_todos)
+        # The weight of each todo is calculated as the inverse of its priority
+        # Lower priorities get higher values, which correspond to greater urgency
+        weights = Enum.map(todos, fn todo -> 1.0 / todo.priority end)
+
+        # Sum all weights to get the total weight of the dataset
+        total_weight = Enum.sum(weights)
+
+        # Generate a random number between 0.0 and 1.0 then multiply that by the total weight
+        # This will give us a random weight that is within the range of the total weight
+        random_weight = :rand.uniform() * total_weight
+
+        # Iterate through the todos, subtracting their weight from the random weight
+        # The current todo is returned when the random weight is less than or equal to the current todo's weight
+        Enum.reduce_while(todos, random_weight, fn todo, acc ->
+          weight = 1.0 / todo.priority
+
+          if acc <= weight do
+            {:halt, todo}
+          else
+            {:cont, acc - weight}
+          end
+        end)
     end
   end
 
